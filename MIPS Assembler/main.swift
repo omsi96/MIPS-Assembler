8baa5a5334bba9a4d919a49374f63cb46dfe494f
OMAR ALIBRAHIM <omsi96@gmail.com>
2019-06-08 16:44:41 +0300

    Initial Commit

---
+++ MIPS Assembler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MIPS Assembler
+//
+//  Created by OMAR ALIBRAHIM on 6/8/19.
+//  Copyright © 2019 OMAR ALIBRAHIM. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
